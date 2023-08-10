3619a33a770a30f7fabf10837936beddac7ff65d
KReddy143 <141957626+KReddy143@users.noreply.github.com>
2023-08-11 01:04:37 +0530

    Add files via upload

---
+++ src/app/data-type.ts
@@ -0,0 +1,53 @@
+export interface SignUp{
+    name:String,
+    password:String,
+    email:String
+}
+
+export interface login{
+    password:String,
+    email:String
+
+}
+
+export interface product{
+    name:string,
+    price:number,
+    category:string,
+    color:string,
+    description:string,
+    image:string,
+    id:number,
+    quantity:undefined | number,
+    productId:undefined | number
+}
+
+export interface cart{
+    name:string,
+    price:number,
+    category:string,
+    color:string,
+    description:string,
+    image:string,
+    id:number | undefined,
+    quantity:undefined | number,
+    userId:number,
+    productId:number
+}
+
+export interface priceSummary{
+    price:number,
+    discount:number,
+    tax:number,
+    delivery:number,
+    total:number
+}
+
+export interface order{
+    email:string,
+    address:string,
+    contact:string,
+    totalPrice:number,
+    userId:number,
+    id:number|undefined
+}
